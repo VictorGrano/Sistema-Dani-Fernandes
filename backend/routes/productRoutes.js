257a46001eaeeb9934156cc6171661003dfa2ca4
Victor Grano <116359890+VictorGrano@users.noreply.github.com>
2024-08-09 10:05:44 -0300

    feat: Atualiza Lote

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,7 +6,7 @@ router.get("/", productController.getAllProducts);
 router.get("/InfoProduto", productController.getInfoProduto);
 router.post("/Cadastrar", productController.getInfoProduto);
 router.put("/Atualizar", productController.updateProduct);
-router.put("/AtualizarLote:lote_id", productController.updateProduct);
+router.put("/AtualizarLote:lote_id", productController.updateLote);
 router.delete("/:id", productController.deleteProduct);
 router.get("/Tipo", productController.getTipo);
 router.get("/Lotes", productController.getLotes);
