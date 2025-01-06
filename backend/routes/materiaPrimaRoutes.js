dfb03a159edd972bc1793b53bd4609d50226e084
Victor Grano <116359890+VictorGrano@users.noreply.github.com>
2025-01-05 23:04:40 -0300

    Alteracao SQL

--- backend/routes/materiaPrimaRoutes.js
+++ backend/routes/materiaPrimaRoutes.js
@@ -5,7 +5,6 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 // Rotas para matérias-primas
 router.get("/", authMiddleware.verifyToken, materiaPrimaController.getMateriasPrimas);
-router.get("/Tipos", authMiddleware.verifyToken, materiaPrimaController.getTiposMateriasPrimas);
 router.get("/Info", authMiddleware.verifyToken, materiaPrimaController.getInfoMateriaPrima);
 router.post("/Cadastro", authMiddleware.verifyToken, materiaPrimaController.postCadastroMateriaPrima);
 router.put("/Atualizar", authMiddleware.verifyToken, materiaPrimaController.updateMateriaPrima);
