3cf0cb07a9e0fa305b992faa8a4e7c37bfadd181
Victor Grano <116359890+VictorGrano@users.noreply.github.com>
2025-05-30 08:17:21 -0300

    Atualizar o config.js

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,12 @@
 //Configuração do banco de dados:
 module.exports = {
-    database: {
-      host: "df_estoque.mysql.dbaas.com.br",
-      user: "df_estoque",
-      password: "D@niAromas16",
-      port: "3306",
-      database: "df_estoque",
-      charset: "utf8mb4",
-    }
-  };
+  database: {
+    host: "SEU_HOST",
+    user: "SEU_USUARIO",
+    password: "SENHA_SEGURA",
+    port: "3306",
+    database: "SEU_BANCO",
+    charset: "utf8mb4",
+  }
+};
   
